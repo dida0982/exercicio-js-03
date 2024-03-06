7f4379e5fb8b9c49d95b1981e75a8f26e30138de
Guilherme Barros JR <131307334+dida0982@users.noreply.github.com>
2024-03-06 16:26:55 -0300

    Add files via upload

---
+++ exercicio-de-js-03/app.js
@@ -0,0 +1,8 @@
+// Defina o saldo da conta
+let saldoConta = 1000; // Exemplo de saldo
+
+// Crie uma mensagem usando uma template string para incluir o valor do saldo
+let mensagemSaldo = `Seu saldo atual é de R$ ${saldoConta.toFixed(2)}.`;
+
+// Exiba a mensagem para o usuário
+alert(mensagemSaldo);
